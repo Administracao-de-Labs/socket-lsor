87df9862df37239984b8c5cd77fc25a7f6892973
Lucas Ferro <lucasferrobrandao@gmail.com>
2026-06-23 20:49:09 -0300

    feat: permite rodar o server com qualquer ip

--- src/socket-server.php
+++ src/socket-server.php
@@ -10,7 +10,7 @@ $messages = [];
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
-socket_bind($socket, '127.0.0.1', 4000);
+socket_bind($socket, '0.0.0.0', 4000);
 
 socket_listen($socket);
 
